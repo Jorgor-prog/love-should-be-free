fa0dac383a50910288ca701b77cd438063b0f114
Jorgor-prog <alpha9274@outlook.com>
2025-08-14 17:29:53 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+// next.config.mjs
+import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { serverActions: { allowedOrigins: ['*'] } },
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+
+  webpack: (config) => {
+    // Алиас '@' указывает на корень проекта
+    config.resolve.alias['@'] = path.resolve(process.cwd());
+    return config;
+  }
 };
+
 export default nextConfig;
